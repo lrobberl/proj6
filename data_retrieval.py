6838aaa5c003426e982ac941fd651ae8c214e4be
emaal <emanuele.alberti@yahoo.it>
2018-10-11 11:48:50 +0200

    Funzione estrazione file corretta, aggiunto file dizionario

--- data_retrieval.py
+++ data_retrieval.py
@@ -2,6 +2,8 @@ import requests
 import json
 import re
 import os
+import gzip
+import shutil
 import time
 import tarfile
 
@@ -81,93 +83,35 @@ def retrieve_data():
 
     file_name = re.findall("filename=(.+)", response_head_cd)[0]
 
-    file_name = "./data_folder/"+file_name
+    # create the first part of the path, starting from the current dir
+    data_folder = os.path.join(os.curdir, "data_folder")
+    file_name = os.path.join(data_folder, file_name)
+    # create dir: if already existing, exist_ok=true suppresses any errors
     os.makedirs(os.path.dirname(file_name), exist_ok=True)
     with open(file_name, "wb") as output_file:
         output_file.write(response.content)
         print("Filename: {}".format(file_name))
 
-    extract_tar_gz(file_name)
+    extract_tar_gz(file_name, data_folder)
 
-def extract_tar_gz(archive_name):
+
+def extract_tar_gz(archive_name, data_folder):
     with tarfile.open(archive_name, "r") as ap:
-        # ap is the archive pointer
+        # ap is the archive pointer. For each directory encountered extract it and analyze it
         for element in ap.getmembers():
-            if element.isdir():
-                print("I'm a dir")
-            ap.next()
+            if element.name != "MANIFEST.txt":
+                # extract dir
+                ap.extract(element.name, data_folder)
+                file_gz_path = os.path.join(data_folder, os.path.join(element.name.split("/")[0], element.name.split("/")[1]))
+                # open and extract the last archive, that contains only the txt file we're interested in
+                file_txt_path = os.path.join(data_folder, os.path.basename(file_gz_path)).split(".gz")[0]
+                try:
+                    # extact the .gz file using the gzip library, opening two fp and copying the content of the 1st into the 2nd
+                    with gzip.open(file_gz_path, 'rb') as f_in, open(file_txt_path, 'wb') as f_out:
+                        shutil.copyfileobj(f_in, f_out)
+                except Exception as e:
+                    print("Generated exception: {}".format(type(e).__name__))
+                shutil.rmtree(os.path.dirname(file_gz_path))
 
-
-def test_esempio():
-    import requests
-    import json
-    import re
-
-    files_endpt = "https://api.gdc.cancer.gov/files"
-
-    filters = {
-        "op": "and",
-        "content": [
-            {
-                "op": "in",
-                "content": {
-                    "field": "cases.project.primary_site",
-                    "value": ["Lung"]
-                }
-            },
-            {
-                "op": "in",
-                "content": {
-                    "field": "cases.demographic.race",
-                    "value": ["white"]
-                }
-            },
-            {
-                "op": "in",
-                "content": {
-                    "field": "cases.demographic.gender",
-                    "value": ["female"]
-                }
-            },
-            {
-                "op": "in",
-                "content": {
-                    "field": "files.analysis.workflow_type",
-                    "value": ["HTSeq - FPKM"]
-                }
-            }
-        ]
-    }
-
-    # Here a GET is used, so the filter parameters should be passed as a JSON string.
-
-    params = {
-        "filters": json.dumps(filters),
-        "fields": "file_id",
-        "format": "JSON",
-        "size": "1000"
-    }
-
-    response = requests.get(files_endpt, params=params)
-
-    file_uuid_list = []
-
-    # This step populates the download list with the file_ids from the previous query
-    for file_entry in json.loads(response.content.decode("utf-8"))["data"]["hits"]:
-        file_uuid_list.append(file_entry["file_id"])
-
-    data_endpt = "https://api.gdc.cancer.gov/data"
-
-    params = {"ids": file_uuid_list}
-
-    response = requests.post(data_endpt, data=json.dumps(params), headers={"Content-Type": "application/json"})
-
-    response_head_cd = response.headers["Content-Disposition"]
-
-    file_name = re.findall("filename=(.+)", response_head_cd)[0]
-
-    with open(file_name, "wb") as output_file:
-        output_file.write(response.content)
-
-def show_file():
-    return
+    # after having extracted the .tar.gz file, remove it
+    os.remove(archive_name)
